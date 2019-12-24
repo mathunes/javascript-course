be3bd195a3c7bfdfee3001e7575bc6e78418c92e
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2019-12-24 12:14:11 -0200

    Adicao dos botoes de inicio e parada

--- intervalos/script.js
+++ intervalos/script.js
@@ -7,4 +7,14 @@ function showTime() {
     document.querySelector("#time").innerHTML = hour+':'+minutes+':'+seconds;
 }
 
+let botao = document.createElement('button');
+document.body.appendChild(botao);
+let iniciar = document.createTextNode('Iniciar')
+botao.appendChild(iniciar);
+
+let botao2 = document.createElement('button');
+document.body.appendChild(botao2);
+let parar = document.createTextNode('Parar')
+botao2.appendChild(parar);
+
 let interval = setInterval(showTime, 1000);
