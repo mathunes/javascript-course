2a2e553b37abe6288ace6c024ba2a35e8dcec41a
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2019-12-24 12:37:46 -0200

    Adicao de funcionalidade utilizando funcao setTimeout

--- intervalos/script.js
+++ intervalos/script.js
@@ -8,11 +8,13 @@ function showTime() {
 }
 
 var interval;
+var timeOut;
 
 let botao = document.createElement('button');
 document.body.appendChild(botao);
 let iniciar = document.createTextNode('Iniciar')
 botao.appendChild(iniciar);
+
 botao.addEventListener('click', function() {
     interval = setInterval(showTime, 1000);
 })
@@ -21,7 +23,19 @@ let botao2 = document.createElement('button');
 document.body.appendChild(botao2);
 let parar = document.createTextNode('Parar')
 botao2.appendChild(parar);
+
 botao2.addEventListener('click', function() {
     clearInterval(interval);
+    clearTimeout(timeOut);
 })
 
+let botao3 = document.createElement('button');
+document.body.appendChild(botao3);
+let pararDepois = document.createTextNode('Parar depois de 5 segundos');
+botao3.appendChild(pararDepois);
+
+botao3.addEventListener('click', function() {
+    timeOut = setTimeout(function() {
+        clearInterval(interval);
+    }, 5000);
+})
