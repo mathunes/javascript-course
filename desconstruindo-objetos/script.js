56ef13b9af462fff785d2d466b112f31ae32b0db
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2019-12-24 12:59:08 -0200

    Convertendo atributos de objetos diretamente em variaveis

---
+++ desconstruindo-objetos/script.js
@@ -0,0 +1,19 @@
+let pessoa = {
+    nome: 'Matheus',
+    sobrenome: 'Antunes',
+    idade: 20,
+    social: {
+        facebook: 'Matheus Antunes',
+        github: 'mathunes'
+    },
+
+    nomeCompleto: function() {
+        return `${this.nome} ${this.sobrenome}`;
+    },
+}
+
+console.log(pessoa.nomeCompleto());
+
+let {nome:pessoaNome = 'Teste', sobrenome, idade} = pessoa;
+
+console.log(pessoaNome);
