512bf17067910b4c99ad9363978870935b0bf249
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2019-12-24 11:25:48 -0200

    Adicao do metodo getFullYear

--- metodos-datas/script.js
+++ metodos-datas/script.js
@@ -10,3 +10,10 @@ console.log(data.toUTCString());
 
 //Converte data para uma string
 console.log(data.toString());
+
+//Pre-definicao da data
+let data2 = new Date('2020/01/01 12:12:12');
+console.log(data2);
+
+//Retorna o ano
+console.log(data.getFullYear());
